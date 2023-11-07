157655a0c80834868731124bc57d7c091c138e30
Elena Shestakova <ShestakovaElenaA@yandex.ru>
2023-11-07 06:26:18 +0300

    replacement asserts

--- src/test/java/ru/netology/OrderingCardTest.java
+++ src/test/java/ru/netology/OrderingCardTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -17,7 +19,7 @@ public class OrderingCardTest {
        $("[data-test-id=phone] input").setValue("+79221052200");
        $("[data-test-id=agreement]").click();
        $(By.className("button")).click();
-       String text = $("[data-test-id=order-success]").getText();
-       Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+       $("[data-test-id=order-success]").shouldBe(visible);
+       $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
